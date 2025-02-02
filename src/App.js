8fd9ba90e2242227f68529533b1a623dde9f4f44
MJ Lynch <mjlynch35@yahoo.com>
2025-02-01 22:09:34 -0600

    Added imageUpload function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [gridSize, setGridSize] = useState(10); // Default grid size
+  const [popularColors, setPopularColors] = useState([]);
+
+  const [showUpload, setShowUpload] = useState(true); // Showing the upload form
+  const canvasRef = useRef(null);
+
+  // Handling the upload of the image
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => setImage(e.target.result);
+      reader.readAsDataURL(file);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
